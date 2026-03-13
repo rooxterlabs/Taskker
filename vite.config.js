2fc871ef50028b166bffe06258ea82b1c73e6c4e
Rodmon <rodmonsevilla2@gmail.com>
2026-03-13 16:37:04 -0700

    v0.010.3

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  /**
-   * We use '/Taskker/' as the base because the current live site 
-   * is hosted at rodmon-rooxter.github.io/Taskker/.
-   * * When the custom domain taskker.io is fully active, 
-   * we will change this to '/'.
-   */
-  base: '/Taskker/',
+  // Setting base to '/' for the final taskker.io custom domain.
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
